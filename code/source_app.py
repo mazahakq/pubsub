a16d2ebb83145d07c11a1ab049c3dfb8b7cead01
mazahakq <mazahakq@devngen.ru>
2025-04-18 02:20:22 +0700

    version 1.0.1

--- code/source_app.py
+++ code/source_app.py
@@ -1,17 +1,18 @@
+import os
 import requests
 import random
 import time
-import logging
+from loguru import logger  # Быстрая библиотека логирования
 
-# Настройка логирования
-logging.basicConfig(level=logging.INFO)
+# Читаем переменную окружения для частоты запросов
+RATE_PER_SECOND = int(os.getenv('RATE_PER_SECOND', '2'))  # По умолчанию 2 запроса/сек
+INTERVAL_BETWEEN_REQUESTS = 1 / RATE_PER_SECOND
 
 # Адрес сервера App A
-url = 'http://app_a:8000/add'
+URL = 'http://app_a:8000/add'
 
-# Частота запросов (2 запроса в секунду)
-rate_per_second = 2
-interval_between_requests = 1 / rate_per_second
+# Настройка логгера Loguru
+logger.add("request_log.log", rotation="1 day", retention="1 week", enqueue=True)
 
 while True:
     # Генерация двух случайных чисел
@@ -23,19 +24,19 @@ while True:
 
     try:
         # Выполняем HTTP-запрос
-        response = requests.post(url, json={"num1": num1, "num2": num2})
+        response = requests.post(URL, json={"num1": num1, "num2": num2})
         response.raise_for_status()  # Проверка статуса ответа
 
         # Измерение времени окончания запроса
         end_time = time.time()
-        elapsed_time = end_time - start_time
+        elapsed_time_ms = (end_time - start_time) * 1000  # Преобразование в миллисекунды
 
-        # Вывод результатов в консоль
-        print(f"Запрос: {num1}+{num2}; Ответ: {response.json().get('result')} ; Время отклика: {elapsed_time:.3f} сек.")
+        # Вывод результатов в консоль и лог
+        logger.info(f"Запрос: {num1}+{num2}; Ответ: {response.json().get('result')} ; Время отклика: {elapsed_time_ms:.3f} ms.")
 
     except requests.RequestException as e:
-        logging.error(f"Ошибка при выполнении запроса: {e}")
+        logger.error(f"Ошибка при выполнении запроса: {e}")
         continue  # Продолжаем следующий цикл
 
     # Задержка между запросами
-    time.sleep(interval_between_requests)
+    time.sleep(INTERVAL_BETWEEN_REQUESTS)
